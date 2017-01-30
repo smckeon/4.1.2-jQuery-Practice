7b762fe6e2a7ebc18b9f92c16f3e13186e031ae0
smckeon <mckeon.sean@outlook.com>
2017-01-30 16:55:30 -0500

    thus far

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
+(function($){
+  "use strict";
 
+$(function(){
+  $('.box-header ul').hide();
+
+  $('.box-header h1').click(function(){
+  $('.box-header ul').slideUp();
+  $(this).next().slideDown();
+    });
+
+
+});
+
+})(jQuery);
